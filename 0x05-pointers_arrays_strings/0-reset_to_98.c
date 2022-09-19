c5aca65e8b93c26e2a63f8559e8932f75f8ea05b
osikoyaop <optimisticopyso@gmail.com>
2022-09-19 15:51:27 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,11 +11,11 @@
 void reset_to_98(int *n);
 {
 	int n;
-	*n = 408;
+	n = 402;
 
 	_putchar("n = %d\n", n);
 	reset_to_98("&n");
 
-	_putchar("*n = %d\n", *n);
+	_putchar("n = %d\n", n);
 	_putchar('\n');
 }
