899d3133d3756609438a94d3bf35cf9d55fd38d3
osikoyaop <optimisticopyso@gmail.com>
2022-09-20 20:48:47 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main,h"
+
+/**
+ * print_array - prints an input of elements
+ * of an array of integers.
+ * @a: an array of integers.
+ *
+ * @n: the numbers of elements to be printed.
+ */
+
+void pint_array(int *a, int n)
+{
+	int i;
+
+	for (i =0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i == n - 1)
+			continue;
+		printf(", ");
+	}
+	pintf("\n");
+}
