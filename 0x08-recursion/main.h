501f4d804571cd18f5cd4de5b1c2ffb3d2e0b8aa
osikoyaop <optimisticopyso@gmail.com>
2022-09-28 06:27:00 -0700

    Tas

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+void _puts_recursion(char *);
+void _print_rev_recursion(char *);
+int _strlen_recursion(char *);
+int factorial(int);
+int wildcmp(char *, char *);
+int is_palindrome(char *);
+int is_prime_number(int);
+int _sqrt_recursion(int);
+int _pow_recursion(int, int);
+
+#endif
