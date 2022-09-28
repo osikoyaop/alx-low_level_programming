bf6181fc21e31f5976d860b842b767fc633f482c
osikoyaop <optimisticopyso@gmail.com>
2022-09-28 06:47:15 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - func that print a string followed by a new line
+ * @s: string to print out
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
