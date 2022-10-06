039c9d17a01c7d85472c78d4087a05ac3bbdd30e
osikoyaop <optimisticopyso@gmail.com>
2022-10-06 11:44:43 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * malloc_checked - func allocates memory using malloc
+ * @b: bytes allocated
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (str == NULL)
+		exit(98);
+	return (ptr);
+}
